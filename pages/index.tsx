e7d7ef9ff5be9748a26589b2c9b125a95076ff92
CrsCaballero <crsecaballero@gmail.com>
2022-07-27 19:58:17 -0700

    Isolate API to fix deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,11 @@
 import React from "react";
-import Link from "next/link";
-import { GetStaticProps } from "next";
 
-import Layout from "@components/Layout/Layout";
-import MainHeader from "@components/Header/MainHeader";
-import ProductList from "@components/ProductList/ProductList";
-
-export const getStaticProps: GetStaticProps = async () => {
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_ENTRYPOINT}/avo`,
+const HomePage = () => {
+	return (
+		<div>
+			<div>AvoStore API</div>
+		</div>
 	);
-	const { data: productList }: TAPIAvoResponse = await response.json();
-
-	return {
-		props: {
-			productList,
-		},
-	};
 };
-
-const HomePage = ({ productList }: { productList: TProduct[] }) => (
-	<Layout>
-		<MainHeader />
-		<section>
-			<Link href="/yes-or-no">
-				<a>Should I eat an avocado today?</a>
-			</Link>
-		</section>
-		<ProductList products={productList} />
-		<style jsx>{`
-			section {
-				text-align: center;
-				margin-bottom: 2rem;
-			}
-		`}</style>
-	</Layout>
-);
 
 export default HomePage;
